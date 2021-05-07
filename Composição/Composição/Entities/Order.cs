0f131ef27ed7441c2ad115f292de5a2308903a46
Gustavo Vieira <gustavovieira.ribeiro@gmail.com>
2021-05-07 12:27:17 -0300

    First commit

---
+++ Composição/Composição/Entities/Order.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Composição.Entities.Enums;
+using System.Globalization;
+
+namespace Composição.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public Client Client { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client, List<OrderItem> items)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+            Items = items;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        
+        public double total()
+        {
+            double sum = 0.0;
+
+            foreach(OrderItem item in Items)
+            {
+                sum += item.subTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items: ");
+            foreach(OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Composição/Composição/Entities/OrderItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Composição.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+        
+        public double subTotal()
+        {
+            return  Quantity * Price;
+        }
+
+        public override string ToString()
+        {
+            return Product.Name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantity: "
+                + ", Subtotal: $"
+                + subTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
