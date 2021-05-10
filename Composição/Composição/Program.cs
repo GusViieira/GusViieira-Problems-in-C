f39cb3568bdc9747aba23cfdfc0ca7774f8b5cbe
Gustavo Vieira <gustavovieira.ribeiro@gmail.com>
2021-05-10 11:10:43 -0300

    Program class edit

--- Composição/Composição/Program.cs
+++ Composição/Composição/Program.cs
@@ -6,7 +6,16 @@ namespace Composição
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter with client data");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.WriteLine("Birth data (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter oder data");
+            Console.Write("Status: ");
+            
         }
     }
 }
